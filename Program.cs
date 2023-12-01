7489147b61aef0f6c5db0811d34abe92e951084f
nicola00 <nicolajobs00@gmail.com>
2023-12-01 09:13:16 +0100

    start csharp

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template sor more information
+// int a = 500;
+// short b = 100;
+// sbyte c = -50;
+// long d = 100_000_000L;
+// int e = 0b10101010;
+// ushort f = 0x0a56;
+
+//Console.WriteLine("a = {0}, b = {1}, c = {2}, d = {3}, e = {4}, f = {5},", a, b, c, d, e, f);
+//Console.WriteLine($"a = {a}, b = {b}, c = {c}, d = {d}, e = {e}, f = {f},");
+
+float f = 12.5f;
+double d = 130.78;
+decimal dm = 1500.456M;
+double ne = 5e-7;
+var s = "type inference";
+var b = false;
+
+Console.WriteLine($"f = {f}, d = {d}, dm = {dm}, ne = {ne}, s = {s}, b = {b}");
